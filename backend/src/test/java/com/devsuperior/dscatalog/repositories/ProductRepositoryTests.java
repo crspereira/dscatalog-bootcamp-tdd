c841c2e3ef1f481f6e41a292642bab58087b8426
Clayton Pereira <clayton.pereira@outlook.com>
2024-02-13 21:03:03 -0300

    BackEnd Cap-2 - TDD Initial Repositories Tests

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.factories.ProductFactory;
+
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	//Injecao Dependencia
+	@Autowired
+	private ProductRepository productRepository;
+
+	//Arrange Compartilhada
+	private long existingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setup() throws Exception {
+		existingId = 1L;
+		nonExistingId = 0L;
+		countTotalProducts = 25;
+	}
+	
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		//Arrange
+		Product product = ProductFactory.createNewProduct();
+		product.setId(null);
+		//Acting
+		product = productRepository.save(product);
+		//Assert
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+	}
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		//Arrange
+		//Long existingId = 1L;
+		//Acting
+		productRepository.deleteById(existingId);
+		Optional<Product> result = productRepository.findById(existingId);
+		//Assert
+		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowsExceptionWhenIdNotExists() {
+		//Arrange
+		//Long nonExistingId = 0L;
+		//Assert
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			//Acting
+			productRepository.deleteById(nonExistingId);
+		});
+	}
+
+}
